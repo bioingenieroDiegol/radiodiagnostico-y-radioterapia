0e0f5190a240a16ecbe92a968d0bb2f692be3e81
Diego Efe <eldiegoefe@gmail.com>
2023-11-09 17:27:46 -0300

    funciones agregadas y más

--- introduccion/funcionesRyR.py
+++ introduccion/funcionesRyR.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy import constants
+from pint import UnitRegistry
+
+u = UnitRegistry()
 
 def N(n0, tmedio, t):
     '''Calcula la exponencial decreciente que describen los núcleos radiactivos al decaer
@@ -78,3 +81,62 @@ def interpolar2(xi,yi,xf,yf,x):
     deltaY = yf - yi
     deltaX = xf - xi
     return (x - xi) * (deltaY / deltaX) + yi
+
+### -------------------------------------
+### Partículas a velocidades relativistas
+### -------------------------------------
+
+def vel(E, m):
+    '''Cálculo de la velocidad (relativista) de una partícula de energía conocida'''
+    c = 300000 * u('km/s')
+    E0 = m*c**2
+    # print(x.to('MeV'))
+    den = (E/E0 + 1)**2
+    # print("denominador: ", den)
+    z = (1 - (1/den))**(1/2)
+    # print("z: ", z)      
+    return c * z
+
+### -------------------------
+### Interacción de electrones
+### -------------------------
+
+def radiacion(Z, Emax):
+    '''Calcula qué proporción de las interacciones de electrones
+    generan radiación de frenado (Brehmstralung).
+    
+    El resultado es en valor absoluto. 
+    Para obtener el porcentaje, se debe multiplicar por 100%.
+    '''
+    return Z * Emax / (3000 * u('MeV'))
+
+def rango_x(rango_agua, densidad_x):
+    '''Devuelve el rango en el material X 
+    cuya densidad es densidad_x'''
+    
+    densidad_agua = 1 * u('g/cm^3')
+    return rango_agua * densidad_agua / densidad_al
+
+
+### interacción de fotones: efecto Compton
+
+def Esc_min(E0, tita):
+    '''Energía del fotón Compton resultante. 
+    El mínimo se da para un ángulo de tita = pi (180 grados)'''
+    E = 511 * u('keV')
+    a = 1 + (E0/E)*(1-np.cos(tita))
+    return E0/a
+
+def Ere(E0, Esc_min):
+    return E0 - Esc_min
+
+def tita(E0, Esc):
+    a = (E0/Esc) - 1 
+    b = 511 * u('keV') / E0
+    return np.arccos(1-a*b)
+
+### Dosimetría
+
+def tasa_x(A, Gamma, r):
+    '''Tasa de exposición para una fuente puntual'''
+    return A * Gamma / r**2
